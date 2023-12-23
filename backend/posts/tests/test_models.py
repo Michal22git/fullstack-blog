460344e27539aa2e78f8ee26f01d4d4f1a7dc123
Michal22git <michalfiszer15@gmail.com>
2023-12-23 19:38:00 +0100

    created models tests

---
+++ backend/posts/tests/test_models.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from posts.models import Post, Like, Comment
+from datetime import datetime
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username="Michal", password="ZAQ!2wsx")
+        self.test_post = Post.objects.create(user=self.test_user, description="Test description")
+
+    def test_post_creation(self):
+        self.assertEqual(self.test_post.user, self.test_user)
+        self.assertEqual(self.test_post.description, "Test description")
+
+    def test_post_ordering(self):
+        post_2 = Post.objects.create(user=self.test_user, description="Another test", created_time=datetime(2023, 1, 1))
+        posts = Post.objects.all()
+        self.assertEqual(posts[0].created_time, post_2.created_time)
+        self.assertEqual(posts[1].created_time, self.test_post.created_time)
+
+
+
+class LikeModelTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username="Michal", password="ZAQ!2wsx")
+        self.test_post = Post.objects.create(user=self.test_user, description="Test description")
+        self.test_like = Like.objects.create(user=self.test_user, post=self.test_post)
+
+    def test_like_creation(self):
+        self.assertEqual(self.test_like.user, self.test_user)
+        self.assertEqual(self.test_like.post, self.test_post)
+
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username="Michal", password="ZAQ!2wsx")
+        self.test_post = Post.objects.create(user=self.test_user, description="Test description")
+        self.test_comment = Comment.objects.create(user=self.test_user, post=self.test_post, content="Test comment very long")
+
+    def test_comment_creation(self):
+        self.assertEqual(self.test_comment.user, self.test_user)
+        self.assertEqual(self.test_comment.post, self.test_post)
+        self.assertEqual(self.test_comment.content, "Test comment very long")
---
+++ backend/users/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from users.models import Profile, Follow
+
+
+class ProfileModelTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username="Michal", password="ZAQ!@WSX")
+        self.test_profile = Profile.objects.create(user=self.test_user, description="Test description of test user")
+
+    def test_profile_str(self):
+        expected_str = f"{self.test_profile.user.username} profile"
+        self.assertEqual(str(self.test_profile), expected_str)
+
+    def test_image_resize(self):
+        profile = self.test_profile
+        img_path = profile.image.path
+
+        profile.save()
+
+        from PIL import Image
+        img = Image.open(img_path)
+        self.assertTrue(img.height <= 300 and img.width <= 300)
+
+
+class FollowerModelTest(TestCase):
+    def setUp(self):
+        self.user_1 = User.objects.create_user(username='user1', password='testpass')
+        self.user_2 = User.objects.create_user(username='user2', password='testpass')
+        self.profile_1 = Profile.objects.create(user=self.user_1)
+        self.profile_2 = Profile.objects.create(user=self.user_2)
+
+    def test_follow(self):
+        follow = Follow.objects.create(user=self.user_1, follower=self.user_2)
+
+        self.assertEqual(Follow.objects.filter(user=self.user_1).count(), 1)
+        self.assertEqual(Follow.objects.filter(follower=self.user_2).count(), 1)
+
+    def test_unfollow(self):
+        follow = Follow.objects.create(user=self.user_1, follower=self.user_2)
+
+        follow.delete()
+
+        self.assertEqual(Follow.objects.filter(user=self.user_1).count(), 0)
+        self.assertEqual(Follow.objects.filter(follower=self.user_2).count(), 0)
